2907e8122f9d60f11085c9b64a9f561186c7c05c
pseudoureka <messiah7179@gmail.com>
2025-01-09 22:30:16 +0900

    가위바위보 imgMap 생성, props 전달

--- src/HandIcon.js
+++ src/HandIcon.js
@@ -1,7 +1,17 @@
 import rockImg from "./assets/rock.svg";
+import scissorImg from "./assets/scissor.svg";
+import paperImg from "./assets/paper.svg";
 
-function HandIcon() {
-  return <img src={rockImg} alt="주먹" />;
+const imgMap = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+
+function HandIcon({ value }) {
+  const src = imgMap[value];
+  const alt = value;
+  return <img src={src} alt={alt} />;
 }
 
 export default HandIcon;

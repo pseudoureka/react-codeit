edcceab04317c1112b493ba336a55e6389e88d69
pseudoureka <messiah7179@gmail.com>
2025-01-09 23:03:59 +0900

    버튼 컴포넌트에 children prop 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Button from "./Button";
 import Dice from "./Dice";
 
 function App() {
   return (
     <>
+      <div>
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
+      </div>
       <Dice color="blue" num={6} />
     </>
   );

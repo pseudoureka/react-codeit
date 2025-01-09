f0f008f93453164f5c7d0f648cfced16476f1554
pseudoureka <messiah7179@gmail.com>
2025-01-09 23:19:17 +0900

    useState 훅 사용, random 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    setNum(Math.floor(Math.random() * 6) + 1);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+  };
+
   return (
     <>
       <div>
-        <Button>던지기</Button>
-        <Button>처음부터</Button>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="blue" num={6} />
+      <Dice color="blue" num={num} />
     </>
   );
 }

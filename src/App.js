e63e3b99b8532c425935087415c6a5d462991686
pseudoureka <messiah7179@gmail.com>
2025-01-10 17:16:57 +0900

    총점 누적 코드 작성

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Dice from "./Dice";
 
 function App() {
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
-    setNum(Math.floor(Math.random() * 6) + 1);
+    const nextNum = Math.floor(Math.random() * 6) + 1;
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
 
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -19,7 +26,14 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="blue" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="blue" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
+      </div>
     </>
   );
 }

a5b25c237e5f9051b8ef8b1919a165783aeddd02
pseudoureka <messiah7179@gmail.com>
2025-01-10 22:48:25 +0900

    인라인 style 적용

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,11 +1,26 @@
 import HandIcon from "./HandIcon";
+import buttonImg from "./assets/purple.svg";
+
+const style = {
+  width: "166px",
+  height: "166px",
+  border: "none",
+  outline: "none",
+  textAlign: "center",
+  cursor: "pointer",
+  backgroundColor: "transparent",
+  backgroundImage: `url(${buttonImg})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "contain",
+};
 
 function HandButton({ value, onClick }) {
   const handleClick = () => {
     onClick(value);
   };
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );

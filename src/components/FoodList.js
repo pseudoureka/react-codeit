1c6af2754c896c1303e34eea304fbe4795406c8e
pseudoureka <messiah7179@gmail.com>
2025-01-12 21:24:23 +0900

    푸드잇 시작, map 메소드로 mock 데이터 출력

---
+++ src/components/FoodList.js
@@ -0,0 +1,30 @@
+import "./FoodList.css";
+
+function FoodListItem({ item }) {
+  const { imgUrl, title, calorie, content } = item;
+
+  return (
+    <div className="FoodListItem">
+      <img className="FoodListItem-img" src={imgUrl} alt={title} />
+      <div>{title}</div>
+      <div>{calorie}</div>
+      <div>{content}</div>
+    </div>
+  );
+}
+
+function FoodList({ items }) {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li>
+            <FoodListItem item={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default FoodList;

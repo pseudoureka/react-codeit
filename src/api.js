8f0fabd26538d8ff0696926e8bad02ff59694df1
pseudoureka <messiah7179@gmail.com>
2025-01-17 13:06:58 +0900

    검색기능 구현

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export async function getFoods({ order = "createdAt", cursor = "", limit = 5 }) {
-  const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
+export async function getFoods({ order = "createdAt", cursor = "", limit = 5, search = "" }) {
+  const query = `order=${order}&cursor=${cursor}&limit=${limit}&search=${search}`;
   const response = await fetch(`https://learn.codeit.kr/7986/foods?${query}`);
   const body = await response.json();
   return body;
--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const [cursor, setCursor] = useState(null);
+  const [search, setSearch] = useState("");
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -29,17 +30,26 @@ function App() {
   };
 
   const handleLoadMore = () => {
-    handleLoad({ order, cursor, limit: LIMIT });
+    handleLoad({ order, cursor, search, limit: LIMIT });
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target["search"].value);
   };
 
   useEffect(() => {
-    handleLoad({ order, cursor: "", limit: LIMIT });
-  }, [order]);
+    handleLoad({ order, cursor: "", search, limit: LIMIT });
+  }, [order, search]);
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
+      <form onSubmit={handleSearchSubmit}>
+        <input name="search" />
+        <button type="submit">검색</button>
+      </form>
       <FoodList items={sortedItems} onDelete={handleDelete} />
       {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
